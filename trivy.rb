7ec8b7ea50154b7ab8fbb14d4c916c67ab3db78b
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-15 17:44:52 +0900

    Brew formula update for trivy version v0.0.12

--- trivy.rb
+++ trivy.rb
@@ -1,9 +1,9 @@
 class Trivy < Formula
   desc ""
   homepage "https://github.com/knqyf263/trivy"
-  url "https://github.com/knqyf263/trivy/releases/download/v0.0.11/trivy_0.0.11_macOS-64bit.tar.gz"
-  version "0.0.11"
-  sha256 "be91c070589d565183e37043572001797afb3438d5e7c177f9d9877ad3bc09c9"
+  url "https://github.com/knqyf263/trivy/releases/download/v0.0.12/trivy_0.0.12_macOS-64bit.tar.gz"
+  version "0.0.12"
+  sha256 "e0e63c25f8101f8a2f9b30948037c3f9fda2193bc728c4921ded957feebd551c"
   
   depends_on "rpm"
 
